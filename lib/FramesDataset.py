7d930fd7de20c09d9c05271ac92ca67f96b540b6
sebbkw <56687947+sebbkw@users.noreply.github.com>
2021-02-06 13:51:08 +0000

    Modularise code

---
+++ lib/FramesDataset.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pickle
+import torch
+
+class FramesDataset (torch.utils.data.Dataset):
+    def __init__ (self, path, split_type, warmup_length):
+        with open(path, 'rb') as file:
+            dataset = pickle.load(file)
+
+        n = len(dataset)
+        splits = {
+            "train": slice(0, int(n*0.8)),
+            "val": slice(int(n*0.8), int(n*0.9)),
+            "test": slice(int(n*0.9), None)
+        }
+
+        dataset = dataset[splits[split_type]]
+        dataset = torch.from_numpy(np.array(dataset))
+        dataset = dataset.type(torch.FloatTensor)
+        
+        self.dataset = dataset
+        self.warmup_length = warmup_length
+        
+    def __len__ (self):
+        return len(self.dataset)
+    
+    def __getitem__ (self, i):
+        window = self.dataset[i]
+        x = window[:self.warmup_length, :]
+        y = window[self.warmup_length:, :]
+
+        return x, y
---
+++ lib/network.py
@@ -0,0 +1,91 @@
+import numpy as np
+import time
+from matplotlib import pyplot as plt
+
+import torch
+import torch.nn as nn
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class RNN (nn.Module):
+    def __init__ (self, hidden_units, frame_size, t_steps):
+        super(RNN, self).__init__()
+        
+        self.hidden_units = hidden_units
+        self.frame_size = frame_size
+        self.t_steps = t_steps
+        
+        self.rnn = nn.RNNCell(
+            input_size = frame_size**2,
+            hidden_size = hidden_units,
+            nonlinearity = 'relu'
+        )     
+        self.fc = nn.Linear(hidden_units, frame_size**2)
+        
+        # Initialise RNN weights with identity matrix
+        self.rnn.weight_hh = torch.nn.Parameter(torch.eye(hidden_units, hidden_units))
+        
+    def forward (self, inputs):
+        predictions = []
+        hidden_state = torch.zeros((inputs.shape[0], self.hidden_units)).to(DEVICE)
+        
+        # Warm up period
+        warmup_length = inputs.shape[1]
+        for i in range(warmup_length):
+            frame_batch = inputs[:, i, :]
+            hidden_state = self.rnn(frame_batch, hidden_state)
+        prediction = self.fc(hidden_state)
+        predictions.append(prediction)
+
+        # Autoregressive predictions
+        for t in range(self.t_steps):
+            hidden_state = self.rnn(prediction, hidden_state)
+            prediction = self.fc(hidden_state)
+            predictions.append(prediction)
+            
+        return torch.transpose(torch.stack(predictions), 0, 1)
+        
+    def save (self, file_name = None):
+        path_name = './models/' + (file_name or 'model-') + time.strftime('%Y%m%d-%H%M%S') + '.pt'
+        torch.save(self.state_dict(), path_name)
+        print('Saved model as ' + path_name)
+    
+    @classmethod
+    def load (cls, hidden_units, frame_size, t_steps, path):
+        model = cls(hidden_units, frame_size, t_steps)
+        model.load_state_dict(torch.load(path))
+        model.eval()
+
+        return model.to(DEVICE)
+    
+def L1_regularisation (lam, loss, model):
+    weights = torch.Tensor([]).to(DEVICE)
+    for name, params in model.named_parameters():
+        if name.endswith('weight'):
+            weights = torch.cat((weights, params.flatten()), 0)
+    
+    return loss + lam*weights.abs().sum()
+
+def plot_input_weights (model, hidden_units, frame_size):
+    weights = model.rnn.weight_ih.cpu().detach().numpy()    
+    weights = weights[:hidden_units, :]
+    
+    im = np.zeros((frame_size, frame_size*hidden_units))
+    for i in range(hidden_units):
+        hidden_unit_weights = weights[i]
+        im[:, i*frame_size:(i+1)*frame_size] = np.reshape(hidden_unit_weights, (frame_size, frame_size))
+    
+    font_size = 5.5
+    font_size_title = 6
+    
+    fig, ax = plt.subplots(dpi=200)
+    ax.imshow(im, extent=[0, hidden_units, 0, 1], cmap='gray')
+    
+    ax.set_xticks(np.arange(0, hidden_units+1))
+    ax.set_xticklabels(np.arange(0, hidden_units+1), size=font_size)
+    ax.set_xlabel('Hidden unit', size=font_size)
+    
+    ax.set_yticks(np.arange(0, 2))
+    ax.set_yticklabels(np.arange(0, 2), size=font_size)    
+    
+    ax.grid(which='major', color='b', linestyle='-', linewidth=1)
