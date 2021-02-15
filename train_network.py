aa58b6266afdfe6a6fc7a69a9b7eb99fcb60414c
sebbkw <56687947+sebbkw@users.noreply.github.com>
2021-02-15 10:46:28 +0000

    Clean up code

--- train_network.py
+++ train_network.py
@@ -5,66 +5,61 @@ import torch.optim as optim
 from lib.FramesDataset import FramesDataset
 from lib import network
 
-WARMUP = 4
-T_STEPS = 45
-FRAME_SIZE = 20
-
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Using", DEVICE)
 
-train_dataset = FramesDataset('./datasets/processed_dataset.npy', 'all', WARMUP)
-train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128)
+hyperparameters = {
+    "framesize": 20,
+    "tsteps": 45,
+    "warmup": 4,
+    "epochs": 2000,
+    "units": 100,
+    "lr": 3*10**-4,
+    "gradclip": 0.25,
+    "L1": 10**-5.75
+}
 
+train_dataset = FramesDataset('./datasets/processed_dataset.npy', 'all', hyperparameters["warmup"])
+train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128)
 print("Training dataset length:", len(train_dataset))
 
-def main ():
-    # Hyperparameters
-    hyperparameters = {
-        "epochs": 2000,
-        "units": 100,
-        "lr": 3*10**-4,
-        "gradclip": 0.25,
-        "L1": 10**-5.75
-    }
+model = network.RecurrentTemporalPrediction(
+    hidden_units = hyperparameters["units"],
+    frame_size = hyperparameters["framesize"],
+    warmup = hyperparameters["warmup"]
+)
+model = model.to(DEVICE)
 
-    model = network.RecurrentTemporalPrediction(
-        hidden_units = hyperparameters["units"],
-        frame_size = FRAME_SIZE, warmup = WARMUP
-    )
-    model = model.to(DEVICE)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=hyperparameters["lr"])
 
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=hyperparameters["lr"])
+loss_history = []
 
-    loss_history = []
+for epoch in range(1, hyperparameters["epochs"] + 1):
+    running_loss = 0
+    loss_i = 0
 
-    for epoch in range(1, hyperparameters["epochs"] + 1):
-        running_loss = 0
-        loss_i = 0
+    for batch_n, data in enumerate(train_data_loader):
+        inputs, targets = data
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
 
-        for batch_n, data in enumerate(train_data_loader):
-            inputs, targets = data
-            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+        optimizer.zero_grad()
+        output, _ = model(inputs)
 
-            optimizer.zero_grad()
-            output, _ = model(inputs)
+        loss = network.L1_regularisation(
+            lam = hyperparameters["L1"],
+            loss = criterion(output, targets),
+            model = model
+        )
+        loss.backward()
+        nn.utils.clip_grad_value_(model.parameters(), hyperparameters["gradclip"])
+        optimizer.step()
 
-            loss = network.L1_regularisation(
-                lam = hyperparameters["L1"],
-                loss = criterion(output, targets),
-                model = model
-            )
-            loss.backward()
-            nn.utils.clip_grad_value_(model.parameters(), hyperparameters["gradclip"])
-            optimizer.step()
+        running_loss += loss.item()
+        loss_i += 1
 
-            running_loss += loss.item()
-            loss_i += 1
+    loss_history.append(running_loss / loss_i)
+    print('Epoch: {}/{}.............'.format(epoch, hyperparameters["epochs"]), end=' ')
+    print("Loss: {:.4f}".format(loss_history[-1]))
 
-        loss_history.append(running_loss / loss_i)
-        print('Epoch: {}/{}.............'.format(epoch, hyperparameters["epochs"]), end=' ')
-        print("Loss: {:.4f}".format(loss_history[-1]))
-
-    model.save(hyperparameters, loss_history)
-
-main()
+model.save(hyperparameters, loss_history)
